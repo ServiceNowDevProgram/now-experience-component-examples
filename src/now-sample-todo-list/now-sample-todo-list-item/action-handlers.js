7c96b4160fe14048a8bebf6fca6b56c3ca6deb0f
srikanth.uppala <srikanth.uppala@servicenow.com>
2020-03-30 19:24:45 +0530

    Updated toggle to check box

--- src/now-sample-todo-list/now-sample-todo-list-item/action-handlers.js
+++ src/now-sample-todo-list/now-sample-todo-list-item/action-handlers.js
@@ -1,14 +1,9 @@
-import { TOGGLE_CLICKED, NOW_BUTTON_CLICKED, UPDATE_TODO, DELETE_TODO } from '../constants';
+import { CHECKBOX_CLICKED, NOW_BUTTON_CLICKED, UPDATE_TODO, DELETE_TODO } from '../constants';
 
 export default {
-    [TOGGLE_CLICKED]: {
-        stopPropagation: true,
-        effect: ({state, action, dispatch}) => {
-            action.preventDefault();
-            const {itemId} = state.properties;
-            const {value} = action.payload;
-            dispatch(UPDATE_TODO, {id: itemId, active: !value});
-        }
+    [CHECKBOX_CLICKED]: ({state, dispatch}) => {
+        const {itemId, itemCompleted} = state.properties;
+        dispatch(UPDATE_TODO, {id: itemId, active: itemCompleted});
     },
     [NOW_BUTTON_CLICKED]: {
         stopPropagation: true,
--- src/now-sample-todo-list/now-sample-todo-list-item/view.js
+++ src/now-sample-todo-list/now-sample-todo-list-item/view.js
@@ -1,15 +1,15 @@
 import '@servicenow/now-toggle';
 import '@servicenow/now-button';
+import { CHECKBOX_CLICKED } from '../constants';
 
-export default (state) => {
+export default (state, {dispatch}) => {
 	const { properties: {itemCompleted, itemLabel} } = state;
-	
+	const itemClass = itemCompleted ? 'checked' : 'unchecked';
+
 	return (
 		<div className="todo-list-item">
-			<now-toggle checked={itemCompleted} size="md"></now-toggle>
-			<label className={itemCompleted ? 'checked' : 'unchecked'}>
-				{itemLabel}
-			</label>
+			<span className="checkbox" on-click={() => dispatch(CHECKBOX_CLICKED)}><i className={itemClass}></i></span>
+			<label className={itemClass}>{itemLabel}</label>
 			<now-button cta="Positive" iconName="" iconSet="solid" label="X" size="sm" variant="secondary"></now-button>
 		</div>
 	)
